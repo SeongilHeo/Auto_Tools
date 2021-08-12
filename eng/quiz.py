24fed7cb00fa581ce16bec2bdd162a5e3393935b
SeongilHeo <tjddlf101@gmail.com>
2021-08-12 18:03:47 +0900

    fix eng error

--- eng/quiz.py
+++ eng/quiz.py
@@ -2,7 +2,7 @@ import time
 
 # Load Data from .txt files as Dictionary 'D'
 
-day=int(input("Enter the data(ex: 2 -> day2)"))
+day=int(input("Enter the data(ex: 2 -> day2): "))
 E=open('data/%02dE.txt'%(day),'r',encoding='utf-8')
 K=open('data/%02dK.txt'%(day),'r',encoding='utf-8')
 D={}
@@ -24,15 +24,15 @@ i=0
 while L:
     i+=1
     each_kor=L.pop(0)
-    print("%s\t\tcount %02d"%(each_kor,len(L)))
-    if s==0 and i==ori:
-        print("----------------------------------")
-        print("review",len(L))
-        s=1
+    print("%s\t\tcount %02d"%(each_kor,len(L)+1))
     if input().strip()!=D[each_kor]:
         L.append(each_kor)
         print("*******"+each_kor+"->"+D[each_kor]+"*******")
         print("*%s*"%(input().strip()==D[each_kor]))
+    if s==0 and i==ori:
+        print("----------------------------------")
+        print("review",len(L))
+        s=1
     print()
 print("Day%02d completed"%day)
 time.sleep(1)
